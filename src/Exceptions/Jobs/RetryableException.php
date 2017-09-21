007ca0a488fedf3140320e76e974a155c7862848
Melissa Aaron <mel@expensify.com>
2017-09-21 12:28:22 -0700

    Set default value for `name` parameter in RetryableException

--- src/Exceptions/Jobs/RetryableException.php
+++ src/Exceptions/Jobs/RetryableException.php
@@ -35,7 +35,7 @@ class RetryableException extends BedrockError
      * @param string     $name     New name for the job
      * @param string     $nextRun  DateTime to retry the job
      */
-    public function __construct($message, $delay = 0, $code = null, Exception $previous = null, string $name, string $nextRun = '')
+    public function __construct($message, $delay = 0, $code = null, Exception $previous = null, string $name = '', string $nextRun = '')
     {
         $code = $code ?? 666;
         $this->delay = $delay;

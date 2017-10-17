35e69a3792c62b31117b807d6c398701fb54d5dd
Tim Golen <tgolen@gmail.com>
2017-10-17 10:02:07 -0600

    Add composer install to travis

--- tools/style.php
+++ tools/style.php
@@ -1,5 +1,6 @@
 #!/usr/bin/env php
 <?php
+declare(strict_types=1);
 require realpath(dirname(__FILE__)).'/../vendor/autoload.php';
 use Expensify\Libs\PHPStyler;
 $styler = new PHPStyler($_SERVER['TRAVIS_BRANCH'], $_SERVER['TRAVIS_COMMIT']);

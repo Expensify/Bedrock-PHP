1115247d7798077fe04390275614951375312a1a
Tim Golen <tgolen@expensify.com>
2017-10-26 09:41:33 -0600

    Add an autoloader and add missing class

--- tools/style.php
+++ tools/style.php
@@ -1,6 +1,7 @@
 #!/usr/bin/env php
 <?php
-require realpath(dirname(__FILE__)).'/../vendor/autoload.php';
+declare(strict_types=1);
+require realpath(dirname(__FILE__)).'/../_autoload.php';
 use Expensify\Libs\PHPStyler;
 $styler = new PHPStyler($_SERVER['TRAVIS_BRANCH'], $_SERVER['TRAVIS_COMMIT']);
 $valid = $styler->check();

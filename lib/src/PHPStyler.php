46d4770306ac7ec20d005ce6e8b2ebc6e11b77d6
Tim Golen <tgolen@expensify.com>
2017-11-01 10:03:17 -0600

    Switching styler logic

--- lib/src/PHPStyler.php
+++ lib/src/PHPStyler.php
@@ -73,13 +73,13 @@ class PHPStyler extends CommandLine
             }
             $fileResult = $this->eexec($fixerCmd, true);
 
-            $fileOK = false;
+            $fileOK = true;
             foreach ($fileResult as $index => $line) {
                 // When a file is fixed, it outputs `   1) File.php` and this is the only way we have to detect if
                 // something was fixed or not as the linter only exits with an error exit code when the fixer actually
                 // fails (not when it fixes something).
-                if (!preg_match('/^\W*1\)/', $line)) {
-                    $fileOK = true;
+                if (preg_match('/^\W*1\)/', $line)) {
+                    $fileOK = false;
                 }
             }
 

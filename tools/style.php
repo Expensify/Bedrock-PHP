e8ae73a54dbac111613bd5c7ffaa04fe6c66447c
Tim Golen <tgolen@expensify.com>
2017-11-01 09:32:33 -0600

    Fix autoloader paths

--- tools/style.php
+++ tools/style.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 declare(strict_types=1);
-require realpath(dirname(__FILE__)).'/vendor/autoload.php';
+require realpath(dirname(__FILE__)).'/../vendor/autoload.php';
 use Expensify\Libs\PHPStyler;
 
 $styler = new PHPStyler($_SERVER['TRAVIS_BRANCH'], $_SERVER['TRAVIS_COMMIT']);

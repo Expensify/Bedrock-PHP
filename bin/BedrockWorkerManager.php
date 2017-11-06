57db30a957280ba3f5c474fa32381b4bce15d803
Matt Bullock <mattabullock@gmail.com>
2017-11-06 14:57:13 -0800

    add more data to log and fix travis

--- bin/BedrockWorkerManager.php
+++ bin/BedrockWorkerManager.php
@@ -390,7 +390,8 @@ function safeToStartANewJob(LocalDB $localDB, int $target, int $maxSafeTime, int
     }
 
     // Don't authorize BWM to call GetJobs
-    $logger->info("Not queueing job, number of running jobs is above the target", ['numActive' => $numActive, 'target' => $target]);
+    $logger->info("Not queueing job, number of running jobs is above the target", ['newBatchAverageTime' => $newBatchAverageTime, 'oldBatchAverageTime' => $oldBatchAverageTime, 'numActive' => $numActive, 'target' => $target]);
+
     return [false, $target];
 }
 

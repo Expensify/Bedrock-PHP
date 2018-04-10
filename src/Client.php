9fa417262f5afa596f0ea83adcb78e97c9c9d38d
Ionatan Wiznia <ionatan@expensify.com>
2018-04-10 16:08:04 +0200

    Allow to pass preferred host

--- src/Client.php
+++ src/Client.php
@@ -307,7 +307,23 @@ class Client implements LoggerAwareInterface
         $rawRequest .= $body;
 
         $response = null;
-        $hostConfigs = $this->getPossibleHosts();
+        $preferredHost = null;
+        if (isset($headers['host'])) {
+            $preferredHost = $headers['host'];
+            unset($headers['host']);
+        }
+        $hostConfigs = $this->getPossibleHosts($preferredHost);
+
+        // If we passed a preferred host and we already had a connected socket, but to a different host and the preferred
+        // host is not blacklisted (the preferred host is returned first in the possible hosts array only when it's not blacklisted)
+        // then we close the socket in order to connect to the preferred one.
+        $closeSocketAfterRequest = false;
+        if ($preferredHost && $this->socket && key($hostConfigs) !== $this->lastHost) {
+            @socket_close($this->socket);
+            $this->socket = null;
+            $closeSocketAfterRequest = true;
+        }
+
         $hostName = null;
         while (!$response && count($hostConfigs)) {
             reset($hostConfigs);
@@ -354,6 +370,12 @@ class Client implements LoggerAwareInterface
 
         if (is_null($response)) {
             throw new ConnectionFailure('Could not connect to Bedrock hosts or failovers');
+        }
+
+        // If we connected to a preferred host, disconnect from it so we don't send all future requests to it.
+        if ($closeSocketAfterRequest) {
+            @socket_close($this->socket);
+            $this->socket = null;
         }
 
         // Log how long this particular call took
@@ -433,9 +455,11 @@ class Client implements LoggerAwareInterface
     }
 
     /**
+     * @param ?string $preferredHost If passed, it will prefer this host over any of the configured ones. This does not
+     *                               ensure it will use that host, but it will try to use it if its not blacklisted.
      * @suppress PhanUndeclaredConstant - suppresses TRAVIS_RUNNING
      */
-    private function getPossibleHosts()
+    private function getPossibleHosts(string $preferredHost = null)
     {
         // We get the host configs from the APC cache. Then, we check the configuration there with the passed
         // configuration and if it's outdated (ie: it has different hosts from the one in the config), we reset it. This
@@ -475,7 +499,7 @@ class Client implements LoggerAwareInterface
         $failoverHostNames = array_keys($this->failoverHostConfigs);
         shuffle($failoverHostNames);
         $mainHostName = array_rand($this->mainHostConfigs);
-        $hostNames = array_merge([$mainHostName], $failoverHostNames);
+        $hostNames = array_unique(array_merge($preferredHost ? [$preferredHost] : [], [$mainHostName], $failoverHostNames));
 
         $nonBlackListedHosts = [];
         foreach ($hostNames as $hostName) {

1bf91793f85fac90b5e0fcb5a3f1a9e0934e5b78
Qadir Haqq <qadir@expensify.com>
2018-11-30 10:03:59 -0800

    completely removed mockRequest !== 1 check

--- bin/BedrockWorkerManager.php
+++ bin/BedrockWorkerManager.php
@@ -308,7 +308,7 @@ try {
 
                             // Open the DB connection after the fork in the child process.
                             try {
-                                if ($worker->getParam("mockRequest") !== '1' && $worker->getParam("mockRequest") !== 'true') {
+                                if ($worker->getParam("mockRequest") !== 'true') {
                                     // Run the worker.  If it completes successfully, finish the job.
                                     $worker->run();
 

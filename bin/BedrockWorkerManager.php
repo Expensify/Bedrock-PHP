82534ff6f5d8a62f1905e35c35b891dfc3a4a3b5
Ionatan Wiznia <ionatan@expensify.com>
2019-08-13 20:49:45 +0200

    Move catch up and only catch bedrock errors

--- bin/BedrockWorkerManager.php
+++ bin/BedrockWorkerManager.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Expensify\Bedrock\Client;
+use Expensify\Bedrock\Exceptions\BedrockError;
 use Expensify\Bedrock\Exceptions\Jobs\DoesNotExist;
 use Expensify\Bedrock\Exceptions\Jobs\IllegalAction;
 use Expensify\Bedrock\Exceptions\Jobs\RetryableException;
@@ -329,6 +330,11 @@ try {
                                     // retried the command in a different server after the first server actually ran the
                                     // command (but we lost the response).
                                     $logger->info('Failed to FinishJob we probably retried the command on a child job so it is safe to ignore', ['job' => $job, 'exception' => $e]);
+                                } catch (BedrockError $e) {
+                                    if (!$job['retryAfter']) {
+                                        throw $e;
+                                    }
+                                    $logger->info('Could not call finishJob successfully, but job has retryAfter, so not failing the job and letting it be processed again');
                                 }
                             } catch (RetryableException $e) {
                                 // Worker had a recoverable failure; retry again later.
@@ -352,18 +358,14 @@ try {
                                     'extraParams' => $extraParams,
                                     'exception' => $e,
                                 ]);
-                                if ($job['retryAfter']) {
-                                    $logger->info('Could not call finishJob successfully, but job has retryAfter, so not failing the job and letting it be processed again');
-                                } else {
-                                    // Worker had a fatal error -- mark as failed.
-                                    try {
-                                        $jobs->failJob($job['jobID']);
-                                    } catch (IllegalAction | DoesNotExist $e) {
-                                        // IllegalAction is returned when we try to finish a job that's not RUNNING, this
-                                        // can happen if we retried the command in a different server
-                                        // after the first server actually ran the command (but we lost the response).
-                                        $logger->info('Failed to FailJob we probably retried a repeat command so it is safe to ignore', ['job' => $job, 'exception' => $e]);
-                                    }
+                                // Worker had a fatal error -- mark as failed.
+                                try {
+                                    $jobs->failJob($job['jobID']);
+                                } catch (IllegalAction | DoesNotExist $e) {
+                                    // IllegalAction is returned when we try to finish a job that's not RUNNING, this
+                                    // can happen if we retried the command in a different server
+                                    // after the first server actually ran the command (but we lost the response).
+                                    $logger->info('Failed to FailJob we probably retried a repeat command so it is safe to ignore', ['job' => $job, 'exception' => $e]);
                                 }
                             } finally {
                                 if ($enableLoadHandler) {

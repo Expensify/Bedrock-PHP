12a20d3ce7b608bbda93b6bb64e0372eeb323a2c
Thomas Wodarek <twodarek@expensify.com>
2025-10-08 15:21:33 -0400

    Add debug messages to php styler

--- ci/style.php
+++ ci/style.php
@@ -2,9 +2,16 @@
 <?php
 
 declare(strict_types=1);
+echo '1';
 require realpath(dirname(__FILE__)).'/../vendor/autoload.php';
+echo '2';
 use Expensify\Bedrock\CI\PHPStyler;
 
+echo '3';
+
 $styler = new PHPStyler($_SERVER['GITHUB_REF'], $_SERVER['GITHUB_SHA']);
+echo '4';
 $valid = $styler->check();
+echo '5';
+echo $valid;
 exit((int) !$valid);

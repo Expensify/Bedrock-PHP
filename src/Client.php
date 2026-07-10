5801c1878cb0509c0c698f440d7d86aa01cc2e62
Ionatan Wiznia <ionatan@expensify.com>
2026-07-10 10:59:10 +0200

    Change to debug instead

--- src/Client.php
+++ src/Client.php
@@ -536,6 +536,8 @@ class Client implements LoggerAwareInterface
             $socketErrorCode = socket_last_error($this->socket);
 
             if ($socketErrorCode === 115) {
+                $this->logger->debug('Bedrock\Client - socket_connect returned error 115, waiting for connection to complete.', ['host' => $host]);
+
                 // Wait for the socket to be ready for writing after EINPROGRESS
                 $write = [$this->socket];
                 $read = [];
@@ -559,6 +561,8 @@ class Client implements LoggerAwareInterface
                 $socketError = socket_strerror($socketErrorCode);
                 throw new ConnectionFailure("Could not connect to Bedrock host $host:$port. Error: $socketErrorCode $socketError");
             }
+        } else {
+            $this->logger->debug('Bedrock\Client - Reusing socket', ['host' => $host, 'cluster' => $this->clusterName, 'pid' => $pid]);
         }
         socket_clear_error($this->socket);
 
@@ -644,6 +648,7 @@ class Client implements LoggerAwareInterface
             $this->getLogger()->info('Bedrock\Client - All possible hosts have been blacklisted, using full list instead');
             $nonBlackListedHosts = $cachedHostConfigs;
         }
+        $this->getLogger()->debug('Bedrock\Client - Possible hosts', ['nonBlacklistedHosts' => array_keys($nonBlackListedHosts)]);
 
         return $nonBlackListedHosts;
     }
